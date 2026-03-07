1e658b98d6c8e58a49c73c5b2fcaf4e2f5265474
jaxenwang <2814388011@qq.com>
2026-03-07 23:55:28 +0800

    implement main structure

--- Server/MagicStreamMovieServer/controllers/movie_controller.go
+++ Server/MagicStreamMovieServer/controllers/movie_controller.go
@@ -1,12 +1,88 @@
 package controllers
 
 import (
+	"context"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/hunter23912/MagicStreamMovies/Server/MagicStreamMovieServer/database"
+	"github.com/hunter23912/MagicStreamMovies/Server/MagicStreamMovieServer/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+
 	"net/http"
+	"time"
 )
+
+var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "List of movies"})
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movies []models.Movie
+		cursor, err := movieCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies"})
+			return
+		}
+		defer cursor.Close(ctx)
+
+		if err := cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies"})
+			return
+		}
+
+		c.JSON(http.StatusOK, movies)
+	}
+}
+
+func GetMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var movie models.Movie
+		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieID}).Decode(&movie)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+		c.JSON(http.StatusOK, movie)
+	}
+}
+
+// AddMovie 添加电影的处理函数
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "validation failed", "details": err.Error()})
+			return
+		}
+		result, err := movieCollection.InsertOne(ctx, movie)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add movie"})
+			return
+		}
+		c.JSON(http.StatusOK, result)
 	}
 }
--- Server/MagicStreamMovieServer/main.go
+++ Server/MagicStreamMovieServer/main.go
@@ -5,16 +5,17 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	controller "MagicStreamMovieServer/controllers"
+	controller "github.com/hunter23912/MagicStreamMovies/Server/MagicStreamMovieServer/controllers"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.LoadHTMLGlob("templates/*.html")
-
 	router.GET("/movies", controller.GetMovies())
+	router.GET("/movie/:imdb_id", controller.GetMovie())
+	router.POST("/addmovie", controller.AddMovie())
 
+	// 为每个http请求启动一个新的goroutine
 	if err := router.Run("localhost:8080"); err != nil {
 		fmt.Println("Failed to start server", err)
 	}
